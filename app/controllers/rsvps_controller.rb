a0fd1cf0d77d9eac0c9302971c1eb5d8b5744d06
Ian Norris <iannorris@Ians-MacBook-Air.local>
2024-10-20 11:46:35 -0700

    remove gallery and fix submissiinos

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -4,13 +4,14 @@ class RsvpsController < ApplicationController
 	end
 
 	def create
-		Person.find_or_initialize_by(email: person_params[:email]).update!(rsvp_status: person_params[:rsvp_status])
+		Person.find_or_initialize_by(email: person_params[:email]).update!(
+			person_params)
 		redirect_to :root, notice: "Thanks for RSVPing!"
 	end
 
 	private
 
 	def person_params
-		params.require(:person).permit(:email, :rsvp_status)
+		params.require(:person).permit(:email, :rsvp_status, :name, :plus_one_counts, :plus_ones, :little_ones, :alcohol_passer_count, :dietary_restrictions)
 	end
 end
